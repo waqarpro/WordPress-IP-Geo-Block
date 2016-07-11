6cc6cd0e1aa49e460464e5dca5795809d4ed4fbb
tokkonopapa <tokkonopapa@yahoo.com>
2016-07-11 23:56:25 +0900

    Improved styles. Removed duplicated functions.

--- ip-geo-block/admin/includes/tab-statistics.php
+++ ip-geo-block/admin/includes/tab-statistics.php
@@ -122,7 +122,7 @@ if ( $options['save_statistics'] ) :
 				'type' => 'html',
 				'option' => $option_name,
 				'field' => $field,
-				'value' => '<table class="'.$option_slug.'-'.$field.'">' .
+				'value' => '<table class="'.$option_slug.'-statistics-table">' .
 					'<thead><tr><th>IPv4</th><th>IPv6</th></tr></thead><tbody><tr>' .
 					'<td>' . esc_html( $statistics['IPv4'] ) . '</td>' .
 					'<td>' . esc_html( $statistics['IPv6'] ) . '</td>' .
@@ -131,7 +131,7 @@ if ( $options['save_statistics'] ) :
 		);
 
 		$field = 'service';
-		$html  = '<table class="'.$option_slug.'-'.$field.'"><thead><tr>';
+		$html  = '<table class="'.$option_slug.'-statistics-table"><thead><tr>';
 		$html .= '<th>' . __( 'Name of API',     'ip-geo-block' ) . '</th>';
 		$html .= '<th>' . __( 'Calls',           'ip-geo-block' ) . '</th>';
 		$html .= '<th>' . __( 'Response [msec]', 'ip-geo-block' ) . '</th>';
@@ -217,7 +217,7 @@ endif;
 		);
 
 		$field = 'cache';
-		$html  = '<table class="'.$option_slug.'-'.$field.'"><thead><tr>';
+		$html  = '<table class="'.$option_slug.'-statistics-table"><thead><tr>';
 		$html .= '<th>' . __( 'IP address',            'ip-geo-block' ) . '</th>';
 		$html .= '<th>' . __( 'Country code / Access', 'ip-geo-block' ) . '</th>';
 		$html .= '<th>' . __( 'Elapsed [sec] / Calls', 'ip-geo-block' ) . '</th>';
